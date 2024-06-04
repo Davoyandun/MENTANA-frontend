52105db8760a0c8cede5c4bfda4cea18b11699b0
Matias Cancina <cancinamatias@gmail.com>
2024-06-04 18:37:15 -0300

    feature/componentes ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { ButtonUI } from '@/shared/components/Button'
 import React from 'react'
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
 			MENTANA
-			<ButtonUI />
 		</main>
 	)
 }
