ed4bd2a8699c370ffdfebaa13d8e08fcdb674ab2
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-06-08 17:15:08 -0300

    instalacion de font family roboto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Navbar from "@/shared/components/navbar/navbar";
 import Banner from "@/shared/components/landing/Banner/banner";
 import StepToUse from "@/shared/components/landing/StepToUse/stepToUse";
 
+
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex bg-whiteSmokeBase min-h-screen flex-col items-center justify-start lg:mt-[100px] mt-[80px] overflow-x-hidden">
+      <main className="font-sans flex bg-whiteSmokeBase min-h-screen flex-col items-center justify-start lg:mt-[100px] mt-[80px] overflow-x-hidden">
         <div className="bannerWrapper flex flex-row md:mt-24 mt-4  justify-center mb-28 md:mb-0">
           <Banner />
         </div>
--- src/shared/components/landing/StepToUse/stepToUse.tsx
+++ src/shared/components/landing/StepToUse/stepToUse.tsx
@@ -8,8 +8,8 @@ export default function StepToUse() {
   return (
     <div className="bg-gradient-to-b from-whiteSmokeBase to-chaarcoal100 min-h-screen w-full flex items-center justify-center">
       <div className="bg-white w-[90%] lg:h-[400px] justify-evenly gap-12 rounded-xl flex flex-col border-l-8 border-solid border-teaRose500 lg:mb-16 mb-32 py-3 lg:py-0">
-        <p className="font-bold text-2xl lg:text-3xl md:ml-11 h-1/4 pt-4 text-chaarcoalBase md:mt-5 self-center md:self-start">Cómo funciona</p>
-        <div className="flex md:hidden lg:flex flex-col md:flex-row h-2/5 md:justify-evenly mb-10 gap-8 md:gap-0">
+        <p className="font-bold text-2xl lg:text-3xl md:ml-11 h-1/4 pt-4 text-chaarcoalBase md:mt-5 lg:mt-12 self-center md:self-start">Cómo funciona</p>
+        <div className="flex md:hidden lg:flex flex-col md:flex-row h-2/5 md:justify-evenly mb-14 gap-8 md:gap-0">
           <CardStepToStep
             image={<svg xmlns="http://www.w3.org/2000/svg" color="#EE818C" fill='none' viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-10">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
