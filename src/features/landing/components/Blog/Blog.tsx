d686f96d020bc24bb45571b8fe924d78d9c806ec
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-06-11 20:55:42 -0300

    creacion de seccion

---
+++ src/features/landing/components/Blog/Blog.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/react-in-jsx-scope */
+'use client'
+
+import CardBlog from './cardBlog'
+
+export default function Blog() {    
+	return (      
+		<div className="bg-gradient-to-b from-whiteSmokeBase to-chaarcoalLight h-full w-full flex items-center justify-center pb-56">
+			<div className="bg-white w-[95%] lg:mb-5 justify-evenly gap-12 rounded-xl flex flex-col  pb-10">
+				<p className="font-bold text-xl text-center mt-6  lg:text-start md:text-3xl md:ml-7 lg:pb-10 text-chaarcoalBase md:mt-5 lg:mt-12 md:self-start">
+                    Últimas entradas del Blog
+				</p>
+				<div className='flex flex-col lg:flex-row w-full gap-10 lg:gap-5 justify-center'>
+					<CardBlog 
+						title='Cómo manejar la ansiedad en tiempos de estrés' 
+						image={'Images/ansiedad.jpeg'} 
+						description='Descubre estrategias efectivas para lidiar con la ansiedad durante momentos dificiles.' 
+						creationDate={'11/06/24'} 
+						author='Dr. Mentana'
+					/>
+					<hr className='hidden lg:flex self-center border-[1px] h-[400px] border-whiteSmoke500'/>
+					<CardBlog 
+						title='Cómo manejar la comunicación en pareja' 
+						image={'Images/comunPareja.jpeg'} 
+						description='Aprende técnicas para fortalecer la conexión y entendimiento con tu pareja.' 
+						creationDate={'11/06/24'} 
+						author='Dr. Mentana'
+					/>
+					<hr className='hidden lg:flex self-center border-[1px] h-[400px] border-whiteSmoke500'/>
+
+					<CardBlog 
+						title='Cómo practicar la mindfulness en la vida diaria' 
+						image={'Images/mindfulness.jpeg'} 
+						description='Descubre técnicas sencillas para incorporar la atención plena en tu rutina.' 
+						creationDate={'11/06/24'} 
+						author='Dr. Mentana'
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/features/landing/pages/landing.tsx
+++ src/features/landing/pages/landing.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable react/react-in-jsx-scope */
-'use client'
 import Banner from '@/features/landing/components/Banner/banner'
 import StepToUse from '@/features/landing/components/StepToUse/stepToUse'
 import Benefits from '@/features/landing/components/Benefits/Benefits'
+import Blog from '@/features/landing/components/Blog/Blog'
 
 export default function Landing() {
 	return (
 		<div>
-			<div className="bannerWrapper flex flex-row md:mt-24 mt-4  justify-center mb-28 md:mb-0">
+			<div className="bannerWrapper flex flex-row md:mt-24 mt-4 justify-center mb-28 md:mb-0">
 				<Banner />
 			</div>
-			<div className="w-full min-h-screen">
-				<StepToUse />
+			<div className='flex flex-col mt-40'>
+				<div className="w-full h-full">
+					<StepToUse />
+				</div>
+				<div className="w-full h-full">
+					<Benefits />
+				</div>
+				<div className="w-full h-full">
+					<Blog />
+				</div>
 			</div>
-			<div className="w-full min-h-screen">
-				<Benefits />
-			</div>
+			
 		</div>
 	)
 }
