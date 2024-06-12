463a4a12844c314b32393b9cb6fba5c9bbbd16f7
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-06-12 00:03:37 -0300

    fix error eslint

--- src/features/landing/components/Benefits/Benefits.tsx
+++ src/features/landing/components/Benefits/Benefits.tsx
@@ -1,31 +1,32 @@
-/* eslint-disable react/react-in-jsx-scope */
 'use client'
+
+import React from 'react' 
 
 export default function Benefits() {
 	return (
 		<div className="bg-gradient-to-b from-chaarcoalLight to-whiteSmokeBase w-full flex items-center justify-center lg:pb-56 pb-28">
 			<div className="bg-white w-[90%] lg:h-[400px] justify-evenly gap-12 rounded-xl flex flex-col border-t-8 lg:border-t-0 lg:border-l-8 border-teaRose500 py-3 lg:py-0">
 				<p className="font-bold text-xl text-center lg:text-start lg:text-3xl md:ml-11 h-1/4 pt-4 text-chaarcoalBase md:mt-5 lg:mt-12 md:self-start">
-					¿Por qué es importante la terapia?
+          ¿Por qué es importante la terapia?
 				</p>
 				<div className="flex flex-col lg:flex-row w-full pb-14">
 					<div className="flex flex-col items-center text-start lg:w-1/2">
 						<div className="w-11/12 lg:w-3/5">
 							<p className="text-chaarcoal700 font-bold text-lg lg:text-[22px] mt-3 items-center text-center lg:text-start">
-								Beneficios de la terapia:
+                Beneficios de la terapia:
 							</p>
 							<div className="text-center lg:text-start">
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Mejora la salud mental y emocional.
+                  ● Mejora la salud mental y emocional.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Desarrolla habilidades de afrontamiento.
+                  ● Desarrolla habilidades de afrontamiento.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Fortalece las relaciones interpersonales.
+                  ● Fortalece las relaciones interpersonales.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Aumenta la autoestima y la confianza.
+                  ● Aumenta la autoestima y la confianza.
 								</p>
 							</div>
 						</div>
@@ -33,20 +34,20 @@ export default function Benefits() {
 					<div className="flex flex-col items-center text-start lg:w-1/2 pt-5 lg:pt-0">
 						<div className="w-11/12 lg:w-3/5">
 							<p className="text-chaarcoal700 font-bold text-lg lg:text-[22px] mt-3 items-center text-center lg:text-start">
-								¿Por qué elegirnos?
+                ¿Por qué elegirnos?
 							</p>
 							<div className="text-center lg:text-start">
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Profesionales certificados y experimentados.
+                  ● Profesionales certificados y experimentados.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Sesiones accesibles desde cualquier lugar.
+                  ● Sesiones accesibles desde cualquier lugar.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Enfoque personalizado y confidencial.
+                  ● Enfoque personalizado y confidencial.
 								</p>
 								<p className="text-slateGrayBase font-semibold text-lg mt-3 lg:w-full">
-									● Métodos terapéuticos actualizados y eficaces.
+                  ● Métodos terapéuticos actualizados y eficaces.
 								</p>
 							</div>
 						</div>
