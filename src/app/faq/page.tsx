713e0d93b1c89681bd7b621fe0f8646577a86a86
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-06-15 23:28:45 -0300

    creacion de componente comeBack

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import Link from 'next/link'
 import FaqContent from './components/faqContent'
+import ComeBack from '@/shared/components/ComeBack/comeBack'
 
 
 export default function Faq() {
 	return (
-		<div className='bg-gradient-to-b from-whiteSmokeBase to-chaarcoalLight min-h-screen flex justify-center'>
-			<div className='pt-36 md:pt-52 pb-40 w-[90%] md:w-[95%] flex flex-col'>
+		<div className='bg-gradient-to-b from-whiteSmokeBase to-chaarcoalLight min-h-screen flex flex-col justify-center'>
+			<div className='pt-36 md:pt-52 pb-40 w-[90%] md:w-[95%] flex flex-col self-center items-center'>
+				<div className='relative bottom-6 md:bottom-8 self-start'>
+					<Link href='/landing'>
+						<ComeBack/>
+					</Link>
+				</div>
+				
 				<FaqContent/>
 			</div>
 		</div>
---
+++ src/shared/components/ComeBack/comeBack.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+export default function ComeBack () {
+	return (
+		<div className='flex flex-row'>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" color='#577C9E' viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-6">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+			</svg>
+			<p className='text-chaarcoal100 font-semibold text-lg underline'>
+							Volver
+			</p>
+		</div>
+	)
+}
