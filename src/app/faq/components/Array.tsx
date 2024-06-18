28b9ab1ed16fb5371ed8c310a2b1f31197ac4f32
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-06-18 13:04:53 -0300

    fix come back

---
+++ src/app/faq/components/Array.tsx
@@ -0,0 +1,43 @@
+// interface Faq {
+//     question: string;
+//     answer: string;
+// }
+
+// export const faqs: Faq[] = [
+// 	{
+// 		question: '¿Con cuánta frecuencia tendré que hacer terapia?',
+// 		answer: 'Tu psicólogo online te recomendará la frecuencia ideal para tus sesiones, generalmente una vez por semana. Sin embargo, la decisión final es tuya. Es importante que, si te comprometes a asistir de manera periódica, lo cumplas para asegurar el éxito de tu proceso terapéutico.'
+// 	},
+// 	{
+// 		question: '¿Cuánto tiempo tendré que estar en terapia?',
+// 		answer: 'El tiempo necesario para la terapia varía según cada persona y sus necesidades específicas.'
+// 	},
+// 	{
+// 		question: '¿Podré tener contacto con mi psicóloga fuera de las sesiones de terapia?',
+// 		answer: 'Sí, puedes contactar con el profesional que te atienda mediante un chat dentro de la plataforma para discutir asuntos relacionados con la terapia, como la entrega de ejercicios. Sin embargo, no se ofrecerá asistencia ni terapia fuera de las sesiones programadas.'
+// 	},
+// 	{
+// 		question: '¿Alguien más sabrá lo que le cuente a mi psicóloga en terapia?',
+// 		answer: 'No, la confidencialidad está garantizada.'
+// 	},
+// 	{
+// 		question: 'Una vez que he reservado mi sesión, ¿puedo modificar el día y la hora o cancelar la sesión?',
+// 		answer: 'Sí, puedes cambiar la fecha o cancelar la sesión con al menos 24 horas de antelación. Si avisas con menos de 24 horas, no aceptaremos cambios. Además, las profesionales esperarán un máximo de 15 minutos desde la hora de inicio de la sesión. Si no te presentas en ese tiempo, la sesión se considerará anulada sin derecho a reembolso. Las cancelaciones no tienen reembolso, así que debes estar segur@ de tu compromiso por asistir.'
+// 	},
+// 	{
+// 		question: '¿Qué necesito para mis sesiones de terapia online?',
+// 		answer: 'Solo necesitas una conexión a internet estable, un dispositivo con cámara y micrófono (como un ordenador, tablet o smartphone), y un lugar tranquilo donde puedas hablar cómodamente.'
+// 	},
+// 	{
+// 		question: '¿Cómo me preparo para mi primera sesión?',
+// 		answer: 'En tu primera sesión, será útil que pienses en los temas que te gustaría abordar. También puedes anotar cualquier pregunta o preocupación que tengas. Esto ayudará a que tu psicólog@ entienda mejor tus necesidades desde el principio.'
+// 	},
+// 	{
+// 		question: '¿Qué pasa si tengo una emergencia psicológica fuera del horario de las sesiones?',
+// 		answer: 'Si tienes una emergencia psicológica, es importante buscar ayuda inmediata en un centro de salud, llamando a un número de emergencia o contactando a un servicio de crisis local. Nuestra plataforma no está diseñada para manejar emergencias fuera del horario de las sesiones programadas.'
+// 	},
+// 	{
+// 		question: '¿Qué métodos de pago aceptan?',
+// 		answer: 'Aceptamos varios métodos de pago, incluyendo tarjetas de crédito y débito, y pagos electrónicos. Todos los pagos se realizan a través de una plataforma segura para garantizar tu privacidad y seguridad.'
+// 	}
+// ]
