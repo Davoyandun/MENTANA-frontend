f59a11d9f7005aed16763a805739c509ad05964a
HDerwyn <hderwynbdo@gmail.com>
2024-06-19 10:42:56 -0400

    added env variables for firebaseConfig

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,16 +1,14 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-
-
 const firebaseConfig = {
-	apiKey: 'AIzaSyBIHqWYQS4fy2-ZWmhKCRhshqVpCZgfbhQ',
-	authDomain: 'mentana-117b3.firebaseapp.com',
-	projectId: 'mentana-117b3',
-	storageBucket: 'mentana-117b3.appspot.com',
-	messagingSenderId: '577507050788',
-	appId: '1:577507050788:web:96c067a67ddbe2c8731fb1',
-	measurementId: 'G-F9ZJRGE5FL'
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
