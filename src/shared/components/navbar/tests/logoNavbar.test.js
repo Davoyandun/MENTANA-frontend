0c3bc99d348ebb99fc96ed6a999a16761a76f3db
HDerwyn <hderwynbdo@gmail.com>
2024-06-21 12:00:16 -0400

    test frontEnd fix update

--- src/shared/components/navbar/tests/logoNavbar.test.js
+++ src/shared/components/navbar/tests/logoNavbar.test.js
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 
-const React = require('react')
-const { render } = require('@testing-library/react')
+import React from 'react'
+import { render } from '@testing-library/react'
 import LogoNavbar from '../logoNavbar'
-// const LogoNavbar = require('../logoNavbar')
 
 test('renders an img tag', () => {
 	const { getByRole } = render(<LogoNavbar />)
